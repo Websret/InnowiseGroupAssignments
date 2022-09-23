3ef74bea3ad7a29d2113486a9472f6f4d733e921
Websret <2244224422az@gmail.com>
2022-09-23 10:58:36 +0300

    Task1

--- Task1.php
+++ Task1.php
@@ -4,20 +4,15 @@ namespace src;
 use http\Exception\InvalidArgumentException;
 
 class Task1{
-    function main(int $inputNumbers): void
+    function main(int $inputNumbers): string
     {
-        try {
-            $output = $inputNumbers > 30 ? "More than 30" : ($inputNumbers > 20 ? "More than 20" : ($inputNumbers > 10 ?
-                "More than 10" : "Equal or less than 10"));
-            echo $output;
-        }catch (\InvalidArgumentException $e){
-            echo "Main function accepts only integers.";
-        }
+        return $inputNumbers > 30 ? "More than 30" : ($inputNumbers > 20 ? "More than 20" : ($inputNumbers > 10 ?
+            "More than 10" : "Equal or less than 10"));
     }
 }
 
 $inputNumbers = new Task1();
 $mainNumbers = "main";
-echo $inputNumbers->$mainNumbers(11);
+echo $inputNumbers->$mainNumbers(23);
 
 ?>
