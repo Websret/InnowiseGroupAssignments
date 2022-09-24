c7ca0e4a155fa769d2456f396a9d978ba99b0b77
Websret <2244224422az@gmail.com>
2022-09-24 23:50:46 +0300

    Task8

--- Task8.php
+++ Task8.php
@@ -3,8 +3,7 @@ namespace src;
 
 class Task8
 {
-    function main(string $js): void
-    {
+    function main(string $js): void{
         $arr = json_decode($js,true);
         array_walk_recursive($arr,function($key,$val){
             echo $val, ":",$key,'
