80facf1b667805df017961fb0c219bcd8b9d6864
Websret <2244224422az@gmail.com>
2022-09-24 23:50:33 +0300

    Task8

---
+++ Task8.php
@@ -0,0 +1,24 @@
+<?php
+namespace src;
+
+class Task8
+{
+    function main(string $js): void
+    {
+        $arr = json_decode($js,true);
+        array_walk_recursive($arr,function($key,$val){
+            echo $val, ":",$key,'
+';
+        });
+    }
+}
+
+$classTask8 = new Task8();
+$json = '{"Title": "The Cuckoos Calling",
+"Author": "Robert Galbraith",
+"Detail": {
+"Publisher": "Little Brown"
+}}';
+$classTask8->main($json);
+
+?>
