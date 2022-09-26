03817b81324058a3728d0be7b9a3e05f5a7d0cdb
Websret <2244224422az@gmail.com>
2022-09-26 23:48:44 +0300

    Task12

--- Task12.php
+++ Task12.php
@@ -1,58 +1,65 @@
 <?php
+
 namespace src;
 
-class MyCalculator{
-    private $num1, $num2, $answer;
+class Task12
+{
+    private $num1;
+    private $num2;
+    private $answer;
 
-    public function __construct($num1, $num2){
+    public function __construct($num1, $num2)
+    {
         $this->num1 = $num1;
         $this->num2 = $num2;
     }
 
-    public function __toString(){
+    public function __toString()
+    {
         return $this->answer;
     }
 
-    public function add(){
+    public function add()
+    {
         $this->answer = $this->num1 + $this->num2;
         return $this;
     }
 
-    public function multiply(){
-         $this->answer = $this->num1 * $this->num2;
-         return $this;
+    public function multiply()
+    {
+        $this->answer = $this->num1 * $this->num2;
+        return $this;
     }
 
-    public function divide(){
+    public function divide()
+    {
         $this->answer = $this->num1 / $this->num2;
         return $this;
     }
 
-    public function subtract(){
+    public function subtract()
+    {
         $this->answer = $this->num1 - $this->num2;
         return $this;
     }
 
-    public function divideBy($number){
+    public function divideBy($number)
+    {
         return $this->answer / $number;
     }
 
-    public function addBy($number){
+    public function addBy($number)
+    {
         return $this->answer + $number;
     }
 
-    public function multiplyBy($number){
+    public function multiplyBy($number)
+    {
         return $this->answer * $number;
     }
 
-    public function subtractBy($number){
+    public function subtractBy($number)
+    {
         return $this->answer - $number;
     }
 }
-
-$mycalc = new MyCalculator(12, 6);
-echo $mycalc->add();
-echo $mycalc->multiply();
-echo $mycalc->add()->divideBy(9);
-
-?>
