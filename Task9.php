52f094140eae7e295bbe5a7ff6d84bd5d361aaf7
Websret <2244224422az@gmail.com>
2022-09-27 00:02:31 +0300

    Task9

--- Task9.php
+++ Task9.php
@@ -1,20 +1,17 @@
 <?php
+
 namespace src;
 
-class Task9{
-    function main(array $arr, int $number):array{
+class Task9
+{
+    public function main(array $arr, int $number): array
+    {
         $output = array();
-        for($i = 0; $i < count($arr) - 2; $i++){
-            if($arr[$i] + $arr[$i+1] + $arr[$i+2] == $number){
+        for ($i = 0; $i < count($arr) - 2; $i++) {
+            if ($arr[$i] + $arr[$i+1] + $arr[$i+2] == $number) {
                 $output[] = "{$arr[$i]}, {$arr[$i+1]}, {$arr[$i+2]}";
             }
         }
         return $output;
     }
 }
-
-$classTask9 = new Task9();
-$arr = [2,7,7,1,8,2,7,8,7];
-var_dump($classTask9->main($arr, 16));
-
-?>
