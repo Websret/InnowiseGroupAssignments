94759ddf93f43c1ca03270e7aa5fba19a23ea6db
Websret <2244224422az@gmail.com>
2022-09-27 00:05:03 +0300

    Task7

--- Task7.php
+++ Task7.php
@@ -1,17 +1,13 @@
 <?php
+
 namespace src;
 
-class Task7{
-    function main(array $arr, int $position):array{
+class Task7
+{
+    public function main(array $arr, int $position): array
+    {
         unset($arr[$position]);
         array_splice($arr, $position, 1);
         return $arr;
     }
 }
-
-$classTask7 = new Task7();
-$arr=[1,2,3,4,5];
-$n = 3;
-var_dump($classTask7->main($arr, $n));
-
-?>
