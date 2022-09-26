12f95090c42dd9bd073d3e8d38cda68107690f2a
Websret <2244224422az@gmail.com>
2022-09-27 00:14:01 +0300

    Task3

--- Task3.php
+++ Task3.php
@@ -1,18 +1,15 @@
 <?php
+
 namespace src;
 
-class Task3{
-    function main(int $number): int
+class Task3
+{
+    public function main(int $number): int
     {
-        if($number > 0){
+        if ($number > 0) {
             return ($number - 1) % 9 + 1;
-        }else{
+        } else {
             return 0;
         }
     }
 }
-
-$classTask3 = new Task3();
-echo $classTask3->main(5689);
-
-?>
