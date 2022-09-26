86ffe28a32e83941285f0de3e21b70fe8ae3fbc2
Websret <2244224422az@gmail.com>
2022-09-27 00:15:28 +0300

    Task2

--- Task2.php
+++ Task2.php
@@ -1,16 +1,13 @@
 <?php
+
 namespace src;
 
-class Task2{
-    function main(string $date): string
+class Task2
+{
+    public function main(string $date): string
     {
         $today = time();
         $diff = (strtotime($date) - $today);
         return (int)($diff/86400);
     }
 }
-
-$classDate = new Task2();
-echo $classDate->main("17-05-2023");
-
-?>
