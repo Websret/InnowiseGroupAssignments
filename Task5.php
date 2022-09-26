e86ea2647ede2540f45f5328bb43115871e2d0b5
Websret <2244224422az@gmail.com>
2022-09-27 00:10:09 +0300

    Task5

--- Task5.php
+++ Task5.php
@@ -1,17 +1,15 @@
 <?php
+
 namespace src;
 
-class Task5{
-    function main (int $n): string{
+class Task5
+{
+    public function main(int $n): string
+    {
         $array = array(0,1);
-        for ($i = 2; $i <= $n; $i++){
+        for ($i = 2; $i <= $n; $i++) {
             $array[$i] = $array[$i-1] + $array[$i-2];
         }
         return (string) $array[$n];
     }
 }
-
-$classTask5 = new Task5();
-echo $classTask5->main(183);
-
-?>
