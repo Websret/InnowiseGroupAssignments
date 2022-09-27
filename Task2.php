a3ca75c6b7d54619bc592ba8661c0f794fff4ecf
Websret <70685435+Websret@users.noreply.github.com>
2022-09-27 21:45:27 +0300

    Update Task2.php

--- Task2.php
+++ Task2.php
@@ -4,11 +4,20 @@ namespace src;
 
 class Task2
 {
-    public function main(string $date): string
+    public function main(string $date): int
     {
         $today = time();
-        $diff = (strtotime($date) - $today);
+        $arrayDate = explode('-', $date);
+        $days = $arrayDate[0];
+        $mounts = $arrayDate[1];
+        $years = $arrayDate[2];
 
-        return (int)($diff / 86400);
+        if (checkdate((int) $mounts, (int) $days, (int) $years)) {
+            $diff = (strtotime($date) - $today) / 86400;
+        } else {
+            throw new \InvalidArgumentException();
+        }
+
+        return ceil($diff);
     }
 }
