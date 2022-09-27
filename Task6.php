4d20d9de80b61f0c00d18a1f71bb335f4f494f74
Websret <70685435+Websret@users.noreply.github.com>
2022-09-27 18:45:34 +0300

    Update Task6.php

--- Task6.php
+++ Task6.php
@@ -6,9 +6,13 @@ class Task6
 {
     public function main(int $year, int $lastYear, int $month, int $lastMonth, string $day = 'Monday'): int
     {
+        if ($year < 1 or $lastYear < 1 or $month < 1 or $lastMonth < 1) {
+            throw new \InvalidArgumentException();
+        }
         $count = 0;
         $startTime = strtotime("01.$month.$year");
-        $endTime = strtotime("31.$lastMonth.$lastYear");
+        $lastDayMonth = cal_days_in_month(CAL_GREGORIAN, $lastMonth, $lastYear);
+        $endTime = strtotime("$lastDayMonth.$lastMonth.$lastYear");
         while ($startTime <= $endTime) {
             if ((date('w', $startTime) == 1) and date('d', $startTime) == 1) {
                 $count++;
