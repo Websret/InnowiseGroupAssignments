6a7e0fc62277e80ecbaf042b61a834d1d42dbd59
Websret <70685435+Websret@users.noreply.github.com>
2022-09-27 14:32:40 +0300

    Update Task5.php

--- Task5.php
+++ Task5.php
@@ -6,10 +6,11 @@ class Task5
 {
     public function main(int $n): string
     {
-        $array = array(0,1);
+        $array = [0, 1];
         for ($i = 2; $i <= $n; $i++) {
-            $array[$i] = $array[$i-1] + $array[$i-2];
+            $array[$i] = $array[$i - 1] + $array[$i - 2];
         }
+
         return (string) $array[$n];
     }
 }
