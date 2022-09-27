823757f29d4d1f03e2345817b6e1eba8c87565be
Websret <70685435+Websret@users.noreply.github.com>
2022-09-27 14:31:25 +0300

    Update Task4.php

--- Task4.php
+++ Task4.php
@@ -6,8 +6,9 @@ class Task4
 {
     public function main(string $input): array|string
     {
-        $chars = array('-', '_');
+        $chars = ['-', '_'];
         $output = ucwords(str_replace($chars, ' ', $input));
+
         return str_replace(' ', '', $output);
     }
 }
