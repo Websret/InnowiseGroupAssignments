f2b01d8dcff76d568e76d04e0340e2e918a11290
Websret <2244224422az@gmail.com>
2022-09-27 19:14:01 +0300

    Task7

--- Task7.php
+++ Task7.php
@@ -6,8 +6,12 @@ class Task7
 {
     public function main(array $arr, int $position): array
     {
+        if ($position > count($arr) - 1) {
+            throw new \InvalidArgumentException();
+        }
         unset($arr[$position]);
-        array_splice($arr, $position, 1);
+        array_splice($arr, $position, 0);
+
         return $arr;
     }
 }
