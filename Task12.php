b62297398f45206f57420631e03f68fb8ec332d3
Websret <2244224422az@gmail.com>
2022-09-27 21:08:34 +0300

    Task12

--- Task12.php
+++ Task12.php
@@ -4,11 +4,11 @@ namespace src;
 
 class Task12
 {
-    private $num1;
-    private $num2;
-    private $answer;
+    private int $num1;
+    private int $num2;
+    private int|float|string $answer;
 
-    public function __construct($num1, $num2)
+    public function __construct(int $num1, int $num2)
     {
         $this->num1 = $num1;
         $this->num2 = $num2;
@@ -19,50 +19,57 @@ class Task12
         return $this->answer;
     }
 
-    public function add()
+    public function add(): Task12
     {
         $this->answer = $this->num1 + $this->num2;
 
         return $this;
     }
 
-    public function multiply()
+    public function multiply(): Task12
     {
         $this->answer = $this->num1 * $this->num2;
 
         return $this;
     }
 
-    public function divide()
+    public function divide(): Task12
     {
+        if ($this->num1 < 1 or $this->num2 < 1) {
+            throw new \InvalidArgumentException();
+        }
         $this->answer = $this->num1 / $this->num2;
 
         return $this;
     }
 
-    public function subtract()
+    public function subtract(): Task12
     {
         $this->answer = $this->num1 - $this->num2;
 
         return $this;
     }
 
-    public function divideBy($number)
+    public function divideBy($number): int|float|string
     {
+        if ($number < 1) {
+            throw new \InvalidArgumentException();
+        }
+
         return $this->answer / $number;
     }
 
-    public function addBy($number)
+    public function addBy($number): int|float
     {
         return $this->answer + $number;
     }
 
-    public function multiplyBy($number)
+    public function multiplyBy($number): int|float
     {
         return $this->answer * $number;
     }
 
-    public function subtractBy($number)
+    public function subtractBy($number): int|float
     {
         return $this->answer - $number;
     }
