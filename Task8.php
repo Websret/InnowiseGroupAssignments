3358980e31960a4aed8e5292c7cb0104963e7d8b
Websret <2244224422az@gmail.com>
2022-09-27 20:06:40 +0300

    Task8

--- Task8.php
+++ Task8.php
@@ -6,9 +6,18 @@ class Task8
 {
     public function main(string $js): void
     {
+        if (!$this->isJson($js)) {
+            throw new \InvalidArgumentException();
+        }
         $arr = json_decode($js, true);
+
         array_walk_recursive($arr, function ($key, $val) {
-            echo $val, ':',$key,"\n";
+            echo "\n",$val, ':',$key;
         });
+    }
+
+    public function isJson($string): bool
+    {
+        return is_string($string) && is_array(json_decode($string, true)) ? true : false;
     }
 }
